543923b6a331191babbb5039a42934bf802e81ad
William F <110934475+unkokaeru@users.noreply.github.com>
2024-05-02 12:38:52 +0100

    Add testing placeholder

---
+++ tests/test_blankslate.py
@@ -0,0 +1,15 @@
+"""test_blankslate.py: Tests for the main entry point of the application."""
+
+
+def test_run_blankslate() -> None:
+    """
+    Test the main entry point of the application.
+
+    Notes
+    -----
+    This function tests the main entry point of the application.
+    It is responsible for testing the application, taking in
+    user input, and generating the project. It does not return
+    anything.
+    """
+    assert True  # Placeholder
