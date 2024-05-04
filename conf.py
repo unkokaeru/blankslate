37612acda6ce6f07aeadb078d8c2cd3031cc52e4
William F <110934475+unkokaeru@users.noreply.github.com>
2024-05-04 16:40:19 +0100

    docs: migrated to sphinx from pdoc

---
+++ conf.py
@@ -0,0 +1,11 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = "blankslate"
+copyright = "2024, William Fayers"
+author = "William Fayers"
+
+extensions = ["sphinx.ext.napoleon"]
+
+templates_path = ["_templates"]
+
+html_theme = "alabaster"
