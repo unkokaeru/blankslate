eac1ceea007638278e68095fad59bd00703997e7
William F <110934475+unkokaeru@users.noreply.github.com>
2024-05-04 20:45:13 +0100

    fix: blankslate-python call for terminal

--- source/blankslate/main.py
+++ source/blankslate/main.py
@@ -1,4 +1,4 @@
-"""___main___.py: Called when the package is run as a script."""
+"""main.py: Called when the package is run as a script."""
 
 from .generation.project_generation import generate_project
 from .interface.command_line import input_project_details
@@ -7,7 +7,15 @@ from .logs.setup_logging import setup_logging
 main_logger = setup_logging()
 
 
-if __name__ == "__main__":
+def main() -> None:
+    """
+    Main function for the application.
+
+    Notes
+    -----
+    This function is the entry point for the application.
+    """
+
     try:
         main_logger.info("Application started.")
         project_details = input_project_details()
@@ -15,3 +23,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\n")
         main_logger.info("Exiting application due to user interrupt...")
+
+
+if __name__ == "__main__":
+    main()
