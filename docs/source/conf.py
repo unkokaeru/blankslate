5afa85d98e5142aeabc37510b7d49bd28dcaa617
William F <110934475+unkokaeru@users.noreply.github.com>
2024-05-05 14:42:48 +0100

    fix: added base url to sphix conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,8 @@ author = "William Fayers"
 
 extensions = ["sphinx.ext.napoleon"]
 
+html_baseurl = "https://blankslate-docs.mydigitalgarden.space/"
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
